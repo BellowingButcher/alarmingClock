543a085be9603a7821c301593c4f4aa6b8db1b4d
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-15 23:20:12 +0000

    adds innerHTML properties to const hrs, mins, secs

--- main.js
+++ main.js
@@ -12,17 +12,22 @@ function displayTime(){
 setInterval(displayTime, 10);
 
 const setAlarmBtn = document.getElementById('setAlarmBtn');
-const hrs = document.getElementById('alarmHour');
-const mins = document.getElementById('alarmMinute');
-const secs = document.getElementById('alarmSecond');
+const hrs = document.getElementById('alarmHour').innerHTML;
+const mins = document.getElementById('alarmMinute').innerHTML;
+const secs = document.getElementById('alarmSecond').innerHTML;
+
 
 
 function setAlarm() {
+    console.log(hrs);
+    console.log(mins);
+    console.log(secs);
 // alarmTime needs to be the values of the inputed options from the drop down menus on the page
     let alarmTime = hrs + mins + secs;
+    console.log(alarmTime);
     let currentTime = Date.now;
 
-    if ((alarmTime - currentTime) === 0) {
+    if ((alarmTime - currentTime) === (0)) {
         alert("Alarming, isn't it?");
     }
 
