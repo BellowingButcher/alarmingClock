fbc644f53a80f131e727fce2f4fbb51d5af8db1f
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-16 05:31:14 +0000

    starts the display time function

--- main.js
+++ main.js
@@ -1,36 +1,4 @@
-function displayTime(){
-    const dateTime = new Date();
-    let hrs = dateTime.getHours();
-    let min = dateTime.getMinutes();
-    let sec = dateTime.getSeconds();
-
-    document.getElementById('hours').innerHTML = hrs;
-    document.getElementById('minutes').innerHTML = min;
-    document.getElementById('seconds').innerHTML = sec;
-
+function timeNow() {
+let displayTime = Date.now
+let displayHour = 
 }
-setInterval(displayTime, 10);
-
-const setAlarmBtn = document.getElementById('setAlarmBtn');
-
-const hrs = document.querySelector('#alarmHour');
-const mins = document.querySelector('#alarmMinute');
-const secs = document.querySelector('#alarmSecond');
-
-
-function setAlarm() {
-console.log(hrs)
-// alarmTime needs to be the values of the inputed options from the drop down menus on the page
-    let alarmTime = hrs + mins + secs;
-    let currentTime = Date.now;
-
-    if ((alarmTime - currentTime) === (0)) {
-        alert("Alarming, isn't it?");
-    }
-
-}
-
-setAlarmBtn.addEventListener('click', setAlarm);
-hrs.addEventListener('change', () => {
-    
-})
