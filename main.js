caaabbe6f8f2f3b99848f8a2217577ad7f89c2b3
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-16 00:08:58 +0000

    works on the const hrs, min, and sec some more

--- main.js
+++ main.js
@@ -12,20 +12,16 @@ function displayTime(){
 setInterval(displayTime, 10);
 
 const setAlarmBtn = document.getElementById('setAlarmBtn');
-/*const hrs = document.getElementById('alarmHour').innerHTML;
-const mins = document.getElementById('alarmMinute').innerHTML;
-const secs = document.getElementById('alarmSecond').innerHTML;*/
-const hrs = document.querySelector('alarmHour').innerHTML;
 
+const hrs = document.querySelector('#alarmHour');
+const mins = document.querySelector('#alarmMinute');
+const secs = document.querySelector('#alarmSecond');
 
 
 function setAlarm() {
-    console.log(hrs);
-    console.log(mins);
-    console.log(secs);
+
 // alarmTime needs to be the values of the inputed options from the drop down menus on the page
     let alarmTime = hrs + mins + secs;
-    console.log(alarmTime);
     let currentTime = Date.now;
 
     if ((alarmTime - currentTime) === (0)) {
