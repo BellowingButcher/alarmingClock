8d0ce71d279e5e1457fbe52cbd069310eafef127
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-16 05:37:54 +0000

    pushes date.now to each hour minute second

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 function timeNow() {
-let displayTime = Date.now
-let displayHour = 
+let displayTime = Date.now;
+let displayHour = document.getElementById('displayHour').innerHTML;
+let displayMinute = document.getElementById('displayMinute').innerHTML;
+let displaySecond = document.getElementById('displaySecond').innerHTML;
 }
