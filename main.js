8dd985539de4d1dd9467817ef204b4513fdd4db3
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-16 07:01:39 +0000

    This fills out the rest of the javascript There are a few errors, but i need help hammering down how to fix it

--- main.js
+++ main.js
@@ -10,3 +10,46 @@ document.getElementById('displaySecond').innerHTML = displaySec;
 
 }
 setInterval (timeNow, 10);
+
+function setAlarm() {
+    let alarmHourTest = false;
+    let alarmMinuteTest = false;
+    let alarmSecondTest = false;
+
+    document.getElementById('alarmHour').addEventListener('click', () => {
+        //I need this to return true when alarmHourTest is called
+    } );
+
+    document.getElementById('alarmMinute').addEventListener('click', () => {
+        //I need this to return true when alarmMinuteTest is called
+    } );
+
+    document.getElementById('alarmHour').addEventListener('click', () => {
+        //I need this to return true when alarmSecondTest is called
+    } );
+
+        if ((alarmHourTest OR alarmMinuteTest OR alarmSecondTest) = false) {
+        alert('You must choose an Hour, Minute, and Second for the alarm to work!');
+    }
+
+        if ((alarmHourTest AND alarmMinuteTest AND alarmSecondTest) = true) {
+        const alarmSetTime = (
+                                (document.querySelector('#alarmHour'))
+                                + (document.querySelector('#alarmMinute'))
+                                + (document.querySelector('#alarmSecond'))
+                             );
+    }
+
+    const displayTimeTest = new Date();
+    let displayTimeTestHr = displayTimeTest.getHours();
+    let displayTimeTestMin = displayTimeTest.getMinutes();
+    let displayTimeTestSec = displayTimeTest.getSeconds();
+
+    const currentTime = displayTimeTestHr + displayTimeTestMin + displayTimeTestSec;
+
+    If (alarmHourTest - displayTimeTest === 0) {
+        alert('It"s alarming, isn"t it?');
+    }
+
+return
+}
